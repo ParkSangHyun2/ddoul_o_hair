bcc4b1953a7b90ce6552ebf2fc5169dc89a9bfdc
parksangpbees <parksang@pbees.net>
2025-06-12 09:45:08 +0900

    카카오맵 바로 랜더링 안되는 문제 수정

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,30 +6,35 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import AddressInfo from "@/components/AddressInfo";
 import NaverMapButton from "@/components/NaverMapButton";
+import {useEffect, useState} from "react";
 
 export default function Contact() {
+    const [scriptLoaded, setScriptLoaded] = useState(false)
+
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const kakao = (window as any).kakao // ← 안전하게 window로부터 가져오기
+
+        if (kakao?.maps) {
+            kakao.maps.load(() => {
+                const mapContainer = document.getElementById('map')
+                if (!mapContainer) return
+
+                const mapOption = {
+                    center: new kakao.maps.LatLng(36.9796, 126.9284),
+                    level: 1,
+                }
+
+                new kakao.maps.Map(mapContainer, mapOption)
+            })
+        }
+    }, [scriptLoaded])
+
     return (<>
         <Script
             strategy="afterInteractive"
             src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=5e3b05819caae8676548c85b7130d015&autoload=false`}
-            onLoad={() => {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                const kakao = (window as any).kakao // ← 안전하게 window로부터 가져오기
-
-                if (kakao?.maps) {
-                    kakao.maps.load(() => {
-                        const mapContainer = document.getElementById('map')
-                        if (!mapContainer) return
-
-                        const mapOption = {
-                            center: new kakao.maps.LatLng(36.9796, 126.9284),
-                            level: 1,
-                        }
-
-                        new kakao.maps.Map(mapContainer, mapOption)
-                    })
-                }
-            }}
+            onLoad={() => setScriptLoaded(true)}
         />
         <main className="min-h-screen flex flex-col items-center text-center px-4 gap-4 pt-[80px]">
             <Header/>
