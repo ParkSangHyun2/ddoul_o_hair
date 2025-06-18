82246e3f165b72cbe747a86c638eedf3871737b6
parksangpbees <parksang@pbees.net>
2025-06-18 16:01:36 +0900

    lightwidget 테스트 완료. 실 배포시 15달러 결제 후 인스타 비즈니스 계정과 연동해야함

--- app/galary/page.tsx
+++ app/galary/page.tsx
@@ -1,26 +1,34 @@
+"use client"
 // app/gallery/page.tsx
 import Hero from "@/components/Hero";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useEffect } from 'react'
 
 export default function GalleryPage() {
-    return (
-        <main className="min-h-screen flex flex-col items-center text-center px-4 gap-2 pt-[80px]">
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://cdn.lightwidget.com/widgets/lightwidget.js';
+        script.async = true;
+        document.body.appendChild(script);
+    });
+
+    return (<main className="min-h-screen flex flex-col items-center text-center px-4 gap-2 pt-[80px]">
             <Header/>
             <Hero title="갤러리" desc="Powered by Instagram"/>
             <div className="flex w-full h-full rounded-lg overflow-hidden shadow-md">
+                {/* 샘플 */}
                 <iframe src="https://snapwidget.com/embed/987654"
                         width="100%" height="600" frameBorder="0" scrolling="yes"
                         allowTransparency={true}></iframe>
-                {/*<iframe*/}
-                {/*    src="https://snapwidget.com/embed/123456" // ← 실제 위젯 코드로 교체하세요*/}
-                {/*    className="w-full h-[600px] sm:h-[800px] border-none"*/}
-                {/*    allowTransparency*/}
-                {/*    scrolling="no"*/}
-                {/*    frameBorder="0"*/}
-                {/*/>*/}
+                {/*{!--LightWidget WIDGET --}*/}
+                {/*<iframe src="//lightwidget.com/widgets/56456f61b5ed59b1bc1efb83fa62d490.html" scrolling="no"*/}
+                {/*        allowTransparency={true} className="lightwidget-widget"*/}
+                {/*        width="100%" height="600"*/}
+                {/*        style={{border:0, overflow:'hidden'}}></iframe>*/}
+
             </div>
             <Footer/>
-        </main>
-    )
+        </main>)
 }
